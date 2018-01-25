1a7060e4bf134d24556101e0d494be9729c2dee2
ed85w <ed.walker@hotmail.co.uk>
2018-01-25 20:46:24 +0000

    with items to pick up

--- adventure.py
+++ adventure.py
@@ -8,6 +8,8 @@ directions = {
 }
 
 position = (0,0)
+posessions = []
+
 
 def map_for_print(player_position):
     adventure_map = {}
@@ -15,8 +17,9 @@ def map_for_print(player_position):
         if pos == player_position:
             adventure_map[pos] = "you are here"
         else:
-            adventure_map[pos] = loc
+            adventure_map[pos] = loc[0]
     return adventure_map
+
 
 def print_map(player_position):
     adventure_map = map_for_print(player_position)
@@ -26,19 +29,39 @@ def print_map(player_position):
         print
 
 
+def collect_item(player_position):
+    if locations[player_position][2]=="Nothing":
+        print "Nothing to collect!"
+    else:
+        posessions.append(locations[player_position][2])
+        print "You have picked up %s" %locations[player_position][2]
+        locations[player_position][2] = "Nothing"
+
+def print_posessions(posessions):
+    if len(posessions) > 1:
+        item_list = " and ".join([str(p) for p in posessions])
+        print "You are holding " + item_list
+    else:
+        print "You are holding " + posessions[0]
+
 
 while True:
     location = locations[position]
-    print "You are at the %s" %location
-
+    print "You are at the %s, %s, at the %s there is %s" %(location[0], location[1], location[0], location[2])
+    if posessions: #if not empty
+        print_posessions(posessions)
+    else:
+        print "You are holding nothing"
     valid_directions = {}
     for k, v in directions.iteritems():
         possible_position = (position[0] + v[0], position[1] + v[1])
         possible_location = locations.get(possible_position)
         if possible_location:
-            print "to the %s is a %s" %(k, possible_location)
+            print "to the %s is a %s" %(k, possible_location[0])
             valid_directions[k] = possible_position
 
+    if raw_input("Pick up the thing? (y/n)").lower() == "y":
+        collect_item(position)
     direction = raw_input('which direction do you want to go?\n')
     if direction in valid_directions:
         position = valid_directions[direction]
--- data.py
+++ data.py
@@ -1,7 +1,9 @@
 locations = {
-    (0, 0): 'house',
-    (0, 1): 'lake',
-    (1, 0): 'park',
-    (1, 1): 'market',
+    (0, 0): ['house','the house is a very nice house','a dog'],
+    (0, 1): ['lake','a watery lake','an oar'],
+    (1, 0): ['park','the park is a park','a swing'],
+    (1, 1): ['market','this place is a rip off','some coins']
 }
 
+map_rows = 2
+map_cols = 2
